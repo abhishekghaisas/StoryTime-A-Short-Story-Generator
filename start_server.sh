0b4a52f89ce458ed6a49a5566609a2aef254098f
Abhishek Ghaisas <abhishekghaisas22@gmail.com>
2025-04-22 01:20:28 -0700

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -2,21 +2,12 @@
 set -e
 
 # Download model files
-python /home/site/wwwroot/download_model.py
+python /home/site/wwwroot/model_setup.py
 
-
-# Download base model if not exists
-if [ ! -d "/home/site/wwwroot/base_model" ]; then
-  python -c "from transformers import AutoModelForCausalLM, AutoTokenizer; \
-    base_model='TinyLlama/TinyLlama-1.1B-Chat-v1.0'; \
-    tokenizer = AutoTokenizer.from_pretrained(base_model); \
-    model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype='auto'); \
-    model.save_pretrained('/home/site/wwwroot/base_model')"
-fi
 
 # Start the application
 cd /home/site/wwwroot
 export MODEL_PATH=/home/site/wwwroot/model
 export TOKENIZERS_PARALLELISM=false
 
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker backend:app --bind=0.0.0.0:8000
+gunicorn --bind=0.0.0.0 --timeout 600 --workers 1 --threads 8 -k uvicorn.workers.UvicornWorker backend:app
