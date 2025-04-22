876fac81890b34c158b16fd8dfccc671466c2abc
Abhishek Ghaisas <abhishekghaisas22@gmail.com>
2025-04-22 00:34:14 -0700

    Add files via upload

---
+++ start_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download base model if not exists
+if [ ! -d "/home/site/wwwroot/base_model" ]; then
+  python -c "from transformers import AutoModelForCausalLM, AutoTokenizer; \
+    base_model='TinyLlama/TinyLlama-1.1B-Chat-v1.0'; \
+    tokenizer = AutoTokenizer.from_pretrained(base_model); \
+    model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype='auto'); \
+    model.save_pretrained('/home/site/wwwroot/base_model')"
+fi
+
+# Start the application
+cd /home/site/wwwroot
+export MODEL_PATH=/home/site/wwwroot/model
+export TOKENIZERS_PARALLELISM=false
+
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker backend:app --bind=0.0.0.0:8000
