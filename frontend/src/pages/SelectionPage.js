3cffda187683c68d866cfdf8cd8164684ba6259b
Abhishek Ghaisas <abhishekghaisas22@gmail.com>
2025-04-23 17:17:45 -0700

    Update SelectionPage.js

--- frontend/src/pages/SelectionPage.js
+++ frontend/src/pages/SelectionPage.js
@@ -1,4 +1,4 @@
-iimport React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const SelectionPage = ({ themes, genres, setStoryData }) => {
